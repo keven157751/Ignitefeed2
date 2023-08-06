093b3135026c67c8541d31ac7aefeab297182997
KevenDev <110134447+keven157751@users.noreply.github.com>
2023-08-06 13:08:58 -0300

    sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
 import { Header } from "./components/header"
 
+import styles from './App.module.css';
+
+import './global.css';
+
 function App() {
   return (
+    <div>
     <Header />
+
+    <div className={styles.wrapper}>
+    <aside>
+      sidebar
+    </aside>
+    <main>
+      <Post 
+      author="Keven Willian" 
+      content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione in aliquam, officiis possimus voluptate asperiores facilis dolore laborum cupiditate magni, officia voluptatum harum. Illo consequatur enim debitis possimus repellendus voluptates." 
+     />
+      <Post 
+      author="Junior templa"  
+      content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo veritatis facilis magni quod hic recusandae ipsum voluptatem aperiam nisi. Eaque, libero dicta. Quibusdam cupiditate quisquam autem eaque nisi possimus hic."
+     />
+      </main>
+     </div>
+    </div>
   )
 }
 
 export default App
+
+
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,10 +1,13 @@
 import styles from './header.module.css'
 
+import ignitelogo from  '../assets/ignite-logo.svg'
+
+console.log(ignitelogo);
 
 export function Header () {
     return (
-    <header className={styles.header}>
-    <strong>Ignite Feed</strong>    
+    <header className={styles.header}> 
+        <img src={ignitelogo} alt="logotipo do ignite" />
      </header>
-    );
+  );
 }

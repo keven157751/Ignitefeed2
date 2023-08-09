735c5d9f857c55c20907ec71c4bc76eb81c8a158
KevenDev <110134447+keven157751@users.noreply.github.com>
2023-08-09 09:31:03 -0300

    commitComment

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,7 +1,36 @@
+import { ThumbsUp, Trash } from '@phosphor-icons/react';
 import styles from './Comment.module.css';
 
 export function Comment() {
     return(
-        <div className=''></div>
+        <div className={styles.comment}>
+            <img className={styles.commentImg} src="https://github.com/keven157751.png" alt="" />
+
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div clasName={styles.authorAndTime}>
+                            <strong>Keven Willian</strong>
+                            <time title='11 de Maio às 08:13h' dateTime='2023-08-08 08:13:30'>Cerca de 1H atrás</time>
+                        </div>
+
+                        <button title='Deletar Comentário'>
+                            <Trash size={20} />
+                        </button>
+
+                    </header>
+
+                    <p>Muito bom Devon, parabéns!!</p>
+                </div>
+
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
     )
 }

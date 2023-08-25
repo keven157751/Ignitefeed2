10aea4f52878d5f1fc997d4d5ce23a07b0035b75
KevenDev <110134447+keven157751@users.noreply.github.com>
2023-08-24 23:12:12 -0300

    teste

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -6,8 +6,14 @@ import { Comment } from './Comment';
 import { Avatar } from './Avatar';
 
 import styles from './Post.module.css';
+import { useState } from 'react';
 
 export function Post({author, publishedAt, content}) {
+    const [comments, setComments] = useState([
+        1,
+        2,
+    ])
+
     const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
         locale: ptBR,
     })
@@ -16,6 +22,12 @@ export function Post({author, publishedAt, content}) {
         locale: ptBR,
         addSuffix: true,
     })
+
+        function handleCreateNewComment() {
+            event.preventDefault()
+
+            setComments([...comments, comments.length + 1]);
+        }
 
     return (
         <article className={styles.post}>
@@ -38,16 +50,16 @@ export function Post({author, publishedAt, content}) {
                     if (line.type === 'paragraph') {
                         return <p>{line.content}</p>;
                     } else if (line.type === 'link') {
-                        return <p></p>
+                        return <p><a href="">{line.content}</a></p>;
                     }
                 })}
             </div>
 
-            <form className={styles.commentForm}>
+            <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
                 <strong>Deixe Seu feedback</strong>
 
                 <textarea
-                    placeholder='Deixe um comentário'
+                    placeholder='Deixe um comentário!'
                 />
                <footer>
                  <button type='submit'>Publicar</button>
@@ -55,9 +67,9 @@ export function Post({author, publishedAt, content}) {
             </form>
 
             <div className={styles.commentList}>
-                <Comment />
-                <Comment />
-                <Comment />
+                {comments.map(comment => {
+                    return <Comment /> 
+                })}
             </div>
 
         </article>

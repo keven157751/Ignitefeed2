807cd9d04f52f9e31b6845064370ed36ea8f1b44
KevenDev <110134447+keven157751@users.noreply.github.com>
2023-08-30 23:41:12 -0300

    request

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,7 +5,7 @@ import ignitelogo from  '../assets/ignite-logo.svg';
 export function Header (aside) {
     return (
     <header className={styles.header}> 
-        <img src={ignitelogo} alt="logotipo do ignites feed!" />
+        <img src={ignitelogo} alt="logotipo do ignites feed" />
      </header>
   );
 }

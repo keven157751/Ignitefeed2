d4f730f8c193b3074cbf855bd675d08b5c46e0fe
KevenDev <110134447+keven157751@users.noreply.github.com>
2023-09-17 20:13:09 -0300

    code identification

--- src/App.jsx
+++ src/App.jsx
@@ -53,15 +53,14 @@ export function App() {
         <Sidebar />
         <main>
           {posts.map(post => {
-            return (<Post
-              key={post.id}
-              author={post.author}
-              content={post.content}
-              publishedAt={post.publishedAt}
-            />);
+              return (<Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />);
           })}
         </main>
-
       </div>
     </div>
   )
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,4 @@
-import { PencilLine } from '@phosphor-icons/react' 
+import { PencilLine } from '@phosphor-icons/react'
 import { Avatar } from './Avatar'
 
 import styles from './Sidebar.module.css';
@@ -7,11 +7,11 @@ import styles from './Sidebar.module.css';
 
 export function Sidebar() {
     return (
-        <aside 
-        className={styles.Sidebar}>
-            <img 
-            className={styles.cover} 
-            src="https://images.unsplash.com/photo-1604964432806-254d07c11f32?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=580&q=80" 
+        <aside
+            className={styles.Sidebar}>
+            <img
+                className={styles.cover}
+                src="https://images.unsplash.com/photo-1604964432806-254d07c11f32?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=580&q=80"
             />
 
             <div className={styles.profile}>
@@ -23,7 +23,7 @@ export function Sidebar() {
 
             <footer>
                 <a href="#">
-                   <PencilLine size={20} />
+                    <PencilLine size={20} />
                     Editar seu perfil:
                 </a>
             </footer>
